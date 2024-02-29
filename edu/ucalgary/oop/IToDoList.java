781c8f4a92a20a5684f4a469d576ea7b67eb5581
hanahassan <hana0hassan0@gmail.com>
2024-02-29 14:30:38 -0700

    new file

---
+++ edu/ucalgary/oop/IToDoList.java
@@ -0,0 +1,8 @@
+package edu.ucalgary.oop;
+
+import java.util.Objects;
+
+public class IToDoList {
+// Define an interface with methods for adding, completing, deleting, editing, undoing, and listing tasks
+
+}
