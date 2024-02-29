b49b8ac074695cfee57361535bb20d16f8dc2a46
hanahassan <hana0hassan0@gmail.com>
2024-02-29 14:33:29 -0700

    changes

--- edu/ucalgary/oop/IToDoList.java
+++ edu/ucalgary/oop/IToDoList.java
@@ -4,5 +4,27 @@ import java.util.Objects;
 
 public class IToDoList {
 // Define an interface with methods for adding, completing, deleting, editing, undoing, and listing tasks
+    public void adding() {
+        
+    }
 
+    public void completing() {
+        
+    }
+    
+    public void deleting() {
+
+    }
+
+    public void editing() {
+        
+    }
+
+    public void undoing() {
+
+    }
+
+    public void listing_tasks() {
+        
+    }
 }
--- edu/ucalgary/oop/Task.java
+++ edu/ucalgary/oop/Task.java
@@ -4,6 +4,10 @@ import java.util.Objects;
 
 public class Task {
 
+    private int id;
+    private String title;
+    private boolean isCompleted;
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
