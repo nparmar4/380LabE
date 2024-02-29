88ccd390b0998dcec83b23e295e352c69e3237b8
hanahassan <hana0hassan0@gmail.com>
2024-02-29 15:00:13 -0700

    changes

--- edu/ucalgary/oop/IToDoList.java
+++ edu/ucalgary/oop/IToDoList.java
@@ -2,43 +2,32 @@ package edu.ucalgary.oop;
 
 import java.util.Objects;
 
-public class IToDoList {
+interface IToDoList {
 
     private List<Task> tasks;
     private Stack<TaskOperation> history;
 
     public IToDoList() {
-        this.tasks = new ArrayList<>();
-        this.history = new Stack<>();
     }
 
-    @Override
     public void addTask(Task task) {
-        tasks.add(task);
-        history.push(new TaskOperation(OperationType.ADD, task));
     }
 
-    @Override
     public void completeTask(Task task) {
-        task.setCompleted(true);
-        history.push(new TaskOperation(OperationType.COMPLETE, task));
     }
 
-    @Override
     public void deleteTask(Task task) {
-        tasks.remove(task);
-        history.push(new TaskOperation(OperationType.DELETE, task));
     }
 
-    public void editing() {
+    public void editTask() {
         
     }
 
-    public void undoing() {
+    public void undo() {
 
     }
 
-    public void listing_tasks() {
+    public void listTasks() {
         
     }
 }
